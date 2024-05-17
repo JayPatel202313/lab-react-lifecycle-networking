b19ac2b4e6efd72e2ba785525a570548b37116e0
JayPatel202313 <jpatel@pursuit.org>
2024-05-17 03:21:46 -0400

    Complete

--- src/Components/EmployeeList.jsx
+++ src/Components/EmployeeList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
+import {useEffect} from 'react';
+import {useState} from 'react';
 
-export const EmployeeList = () => {
+export const EmployeeList = ({ records }) => {
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {records.map(record => (
+          <Employee key={record.id} employee={record} />
+        ))}
       </section>
     </main>
   );
